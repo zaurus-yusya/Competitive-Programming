b6d2a61731aaf9053209f5ecc2aafe1eb3228cd4
zaurus_yusya <54097621+zaurus-yusya@users.noreply.github.com>
2019-08-31 22:40:38 +0900

    commit 8_31

--- template.cpp
+++ template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for(int i=0;i<(n);i++)
+#define repr(i,n) for(int i=(n-1);i>=0;i--)
 #define pb push_back
 #define mp make_pair
 #define all(x) x.begin(),x.end()

0dcd1e0fb20797af2018089a94042fd3e3557d7a
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2019-12-01 00:24:22 +0900

    20191130

--- contest/arc/arc031/b.cpp
+++ contest/arc/arc031/b.cpp
@@ -7,9 +7,73 @@ typedef long long ll;
 #define all(x) x.begin(),x.end()
 using namespace std;
 
+bool seen[10][10];
+vector<vector<char>> vec(10, vector<char>(10));
+vector<int> next_y = {-1,1,0,0};
+vector<int> next_x = {0,0,-1,1};
+
+void dfs(ll x, ll y)
+{
+    seen[x][y] = true;
+
+    rep(i,4){
+        ll new_x = x + next_x.at(i);
+        ll new_y = y + next_y.at(i);
+
+        if(new_x < 0 || new_x >= 10 || new_y < 0 || new_y >= 10){
+            continue;
+        }
+
+        if(vec.at(new_x).at(new_y) == 'x' || seen[new_x][new_y] == true){
+            continue;
+        }
+
+        dfs(new_x, new_y);
+    }
+
+}
 
 int main() {
+    string s;
+
+    for(ll i=0;i<10;i++){
+        cin >> s;
+        for(ll j=0;j<10;j++){
+            vec.at(i).at(j) = s.at(j);
+            if(vec.at(i).at(j) == 'o');
+        }
+    }
+
+    string flag;
+    for(ll i=0;i<10;i++){
+        for(ll j=0;j<10;j++){
+            //seenを初期化
+            rep(x,10){
+                rep(y,10){
+                    seen[x][y] = false;
+                }
+            }
+
+            dfs(i,j);
+        
+            flag = "YES";
+            rep(x,10){
+                rep(y,10){
+                    if(vec.at(x).at(y) == 'o'){
+                        if(seen[x][y] == false){
+                            flag = "NO";
+                        }
+                    }
+                }
+            }
+            if(flag == "YES"){
+                cout << "YES" << endl;
+                return 0;
+            }
+
+        }
+    }
+
+    cout << "NO" << endl;
     
-
-
 }
--- strategy/dfs.cpp
+++ strategy/dfs.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(n);i++)
-#define repr(i,n) for(int i=(n-1);i>=0;i--)
+typedef long long ll;
+#define rep(i,n) for(ll i=0;i<(n);i++)
+#define repr(i,n) for(ll i=(n-1);i>=0;i--)
 #define pb push_back
 #define mp make_pair
 #define all(x) x.begin(),x.end()
 using namespace std;
-typedef long long ll;
+ll INF = 1000000007;
 using Graph = vector<vector<ll>>;
 
 //深さ優先探索(グラフを用いたもの)
@@ -13,11 +14,11 @@ using Graph = vector<vector<ll>>;
 //通ったか通ってないかを判定する配列(mainでfalseに初期化)
 vector<bool> seen;
 
-void dfs(const Graph &G, int v)
+void dfs(const Graph &G, ll v)
 {
     seen[v] = true;
 
-    for(int next_node : G[v]){
+    for(ll next_node : G[v]){
         if(seen[next_node]){
             continue;
         }else{
@@ -32,12 +33,12 @@ void dfs(const Graph &G, int v)
 
 int main() {
     //n:頂点の数, m:辺の数
-    int n, m;
+    ll n, m;
     cin >> n >> m;
 
     Graph G(n);
-    for (int i = 0; i < m; ++i) {
-        int a, b;
+    for (ll i = 0; i < m; ++i) {
+        ll a, b;
         cin >> a >> b;
 
         //有向グラフ
@@ -46,9 +47,27 @@ int main() {
         //無向グラフ
         G[a].push_back(b); G[b].push_back(a);
 
-        seen.assign(n,false);
-        dfs(G, 0);
-
     }
+
+    /////////////////
+    //出来上がった木を確認する
+    for(ll i = 0; i < G.size(); i++){
+        cout << "node " << i << " ";
+        cout << "{";
+        try{
+            for(ll j = 0; j < G[i].size(); j++){
+                if(j == G[i].size() - 1) cout << G[i].at(j);
+                else cout << G[i].at(j) << ", ";
+            }
+            cout << "}" << endl;
+        }catch (std::out_of_range)
+        {
+            cout << "}" << endl;
+        }   
+    }
+    /////////////////
+
+    seen.assign(n,false);
+    dfs(G, 0);
 
 }

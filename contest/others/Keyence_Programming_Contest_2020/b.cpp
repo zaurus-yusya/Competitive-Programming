9faf4e806af5e3bec9ccabf6b41c50da8da1ed31
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2020-01-19 17:44:34 +0900

    20200119

--- contest/others/Keyence_Programming_Contest_2020/b.cpp
+++ contest/others/Keyence_Programming_Contest_2020/b.cpp
@@ -33,79 +33,27 @@ int main() {
     vector<pair<ll,ll>> vec2(n);
 
     rep(i,n){
-        vec2.at(i).first = vec.at(i).first - vec.at(i).second;
-        vec2.at(i).second = vec.at(i).first + vec.at(i).second;
+        vec2.at(i).first = vec.at(i).first + vec.at(i).second;
+        vec2.at(i).second = vec.at(i).first - vec.at(i).second;
     }
 
     sort(vec2.begin(), vec2.end());
 
-    cout << "----" << endl;
+    ll ans = 0;
+    ll tmp = 0;
     rep(i,n){
-        cout << vec2.at(i).first << " " << vec2.at(i).second <<endl;
-    }
-
-    cout << "----" << endl;
-
-    vector<ll> robot(n);
-    ll tmp = -1 * INF;
-    ll ans4 = 0;
-    rep(i,n){
-        if(robot[i] == 1) continue;
-        if(i == n-1){
-            //cout << "ok " <<endl;
-            if(vec2[i].first >= tmp){
-                //cout << i << endl;
-                ans4++;
-            }
+        if(i == 0){
+            ans++;
+            tmp = vec2.at(i).first;
             continue;
         }
-
-        if(vec2[i].second > vec2[i+1].first){
-            if(vec2[i].second > vec2[i+1].second){
-                robot[i] = 1;
-            }else{
-                robot[i+1] = 1;
-                //cout << i << endl;
-                ans4++;
-                tmp = vec2[i].second;
-            }
-        }else{
-            //cout << i << endl;
-            ans4++;
-            tmp = vec2[i].second;
-        }
-    }
-
-    cout << ans4 << endl;
-
-    vector<ll> robot2(n);
-    tmp = -1 * INF;
-    ll ans = 0;
-    rep(i,n){
-        if(i == 0) continue;
-
-        if(tmp > vec2[i].first){
-            cout << "in " << endl;
-            if(vec2[i-1].second < vec2[i].second){
-                tmp = vec2[i-1].second;
-                cout << "i= " <<i <<endl;
-                robot2[i] = 1;
-            }else{
-                tmp = vec2[i].second;
-                cout << "i=" <<i <<endl;
-                robot2[i-1] = 1;
-            }
-        }
-
-    }
-
-    cout << "---" << endl;
-    rep(i,n){
-        if(robot2.at(i) == 0){
-            cout << i << endl;
+        if(tmp <= vec2.at(i).second){
+            tmp = vec2.at(i).first;
             ans++;
         }
     }
+
     cout << ans << endl;
+    
     
 }

c8984bd729ad21f1974db63fa262127363f9cf6c
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2020-05-25 00:00:00 +0900

    20200524

--- atcoder/agc/agc044/b.cpp
+++ atcoder/agc/agc044/b.cpp
@@ -18,12 +18,24 @@ template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;}
 // ceil(a)  1.2->2.0
 // c++17	g++ -std=c++17 a.cpp
 
+vector<int> next_y = {-1,1,0,0};
+vector<int> next_x = {0,0,-1,1};
+
 int main() {
     std::cout << std::fixed << std::setprecision(15);
-    ll T; cin >> T;
-    rep(t, T){
-        ll n, a, b, c, d;
-        cin >> n >> a >> b >> c >> d;
+    ll n; cin >> n;
+    vector<long long> vec(n*n);
+    for(long long i = 0; i < n*n; i ++){
+        cin >> vec[i];
+    }
+
+    vector<vector<string>> grid(n, vector<string>(n, "#"));
+
+    rep(i, n*n){
+        vector<vector<ll>> dist(h, vector<ll>(w, -1));
+
+
 
     }
+
 }

813ae69c047a742ab041dfc086534a9ad82d565a
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2020-09-14 00:33:23 +0900

    20200914

--- atcoder/abc/abc156/a.cpp
+++ atcoder/abc/abc156/a.cpp
@@ -12,31 +12,25 @@ using Graph = vector<vector<ll>>;
 using pll = pair<ll, ll>;
 template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
 template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
+ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b == 0) (a/b) + 1
+ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cout << v[i] << " ";} br;}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cout << v[i][j] << " ";} br;}}
 
 // 0 false, 1 true 
-// string to int : -48
+// string number to int : -48
 // a to A : -32
 // ceil(a)  1.2->2.0
 // c++17	g++ -std=c++17 a.cpp
-
-ld calc(ld x, ld p){
-    return x + p * powl(2, -(x/1.5));
-}
+// global vector -> 0 initialization
 
 int main() {
     std::cout << std::fixed << std::setprecision(15);
-    ld p; cin >> p;
-    ld l = 0, r = 1e18; 
-    ld midl, midr;
-    while(r - l > pow(10, -8)){
-        midl = (l*2 + r) / 3;
-        midr = (l + r*2) / 3;
-        if(calc(midl, p) < calc(midr, p)){
-            r = midr;
-        }else{
-            l = midl;
-        }
+    ll n, r; cin >> n >> r;
+    if(n >= 10){
+        cout << r << endl;
+    }else{
+        cout << r + 100*(10-n) << endl;
     }
-    cout << calc(l, p) << endl;
 
 }
--- atcoder/abc/abc156/d.cpp
+++ atcoder/abc/abc156/d.cpp
@@ -1,77 +1,154 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+typedef long double ld;
 #define rep(i,n) for(ll i=0;i<(n);i++)
 #define repr(i,n) for(ll i=(n-1);i>=0;i--)
-#define pb push_back
-#define mp make_pair
 #define all(x) x.begin(),x.end()
-#define br cout << endl;
+#define br cout << "\n";
 using namespace std;
-const int INF = 1e9;
-const int MOD = 1e9+7;
+const long long INF = 1e10;
+const long long MOD = 1e9+7;
 using Graph = vector<vector<ll>>;
+using pll = pair<ll, ll>;
 template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
 template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
+ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b == 0) (a/b) + 1
+ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cout << v[i] << " ";} br;}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cout << v[i][j] << " ";} br;}}
 
 // 0 false, 1 true 
-// stringの数字をint型にしてアスキーコードになったら -48する
-// 切り上げ　ceil(a)
-// コンパイル　g++ -std=c++17 a.cpp
+// string number to int : -48
+// a to A : -32
+// ceil(a)  1.2->2.0
+// c++17	g++ -std=c++17 a.cpp
+// global vector -> 0 initialization
 
-//(a / b) % MOD : a * modinv(b, MOD) % MOD
-long long modinv(long long a, long long mod) {
-    long long b = mod, u = 1, v = 0;
-    while (b) {
-        long long t = a / b;
-        a -= t * b; swap(a, b);
-        u -= t * v; swap(u, v);
+//https://atcoder.jp/contests/abc172/submissions/14765570
+//20200724
+template<long long mod>
+struct ModInt
+{
+    long long x;
+    ModInt () : x(0) {}
+    ModInt (long long x) : x(x >= 0 ? x % mod : (mod - -x % mod) % mod) {}
+    ModInt &operator += (const ModInt &p){
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
     }
-    u %= mod;
-    if (u < 0) u += mod;
-    return u;
-}
-
-//a^n % mod
-long long modpow(long long a, long long n, long long mod){
-    ll res = 1;
-    while(n > 0){
-        if(n & 1){
-            res = res * a % mod;
+    ModInt &operator -= (const ModInt &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    ModInt &operator *= (const ModInt &p) {
+        x = (long long) x * p.x % mod;
+        return *this;
+    }
+    ModInt &operator /= (const ModInt &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    ModInt &operator ^= (long long p) {
+        ModInt res = 1;
+        for (; p; p >>= 1) {
+            if (p & 1) res *= *this;
+            *this *= *this;
         }
-        a = a * a % mod;
-        n >>= 1;
+        return *this = res;
     }
-    return res;
-}
-
-
-ll modncr(ll n, ll r){
-    ll bunshi = 1;
-    ll bunbo = 1;
-    for(ll i = 0; i < r; i++){
-        bunshi = (bunshi * (n-i)) % MOD;
-        bunbo = (bunbo * (i+1)) % MOD;
+    ModInt operator - () const { return ModInt(-x); }
+    ModInt operator + (const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator - (const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator * (const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator / (const ModInt &p) const { return ModInt(*this) /= p; }
+    ModInt operator ^ (long long p) const { return ModInt(*this) ^= p; }
+    bool operator == (const ModInt &p) const { return x == p.x; }
+    bool operator != (const ModInt &p) const { return x != p.x; }
+    explicit operator long long() const { return x; }
+    ModInt &operator = (const long long p) { x = p; return *this;}
+    ModInt inverse() const {
+        long long a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            a -= t * b;
+            std::swap(a, b);
+            u -= t * v;
+            std::swap(u, v);
+        }
+        return ModInt(u);
     }
-    return (bunshi * modinv(bunbo, MOD)) % MOD;
-}
+    friend std::ostream & operator << (std::ostream &stream, const ModInt<mod> &p) {
+        return stream << p.x;
+    }
+    friend std::istream & operator >> (std::istream &stream, ModInt<mod> &a) {
+        long long x;
+        stream >> x;
+        a = ModInt<mod>(x);
+        return stream;
+    }
+};
 
+template<long long mod> struct MComb {
+    using mint = ModInt<mod>;
+    std::vector<mint> fact;
+    std::vector<mint> inv;
+    MComb (long long n) { // O(n + log(mod))
+        fact = std::vector<mint>(n + 1, 1);
+        for (long long i = 1; i <= n; i++) fact[i] = fact[i - 1] * mint(i);
+        inv.resize(n + 1);
+        inv[n] = fact[n] ^ (mod - 2);
+        for (long long i = n; i--; ) inv[i] = inv[i + 1] * mint(i + 1);
+    }
+    mint ncr(long long n, long r) {
+        return fact[n] * inv[r] * inv[n - r];
+    }
+    mint npr(long n, long r) {
+        return fact[n] * inv[n - r];
+    }
+    mint nhr(long n, long r) {
+        assert(n + r - 1 < (long long) fact.size());
+        return ncr(n + r - 1, r);
+    }
+};
+
+template<long long mod> struct MPow {
+    using mint = ModInt<mod>;
+    mint modpow(mint a, long long n) {
+        mint res = 1;
+            while(n > 0){
+            if(n & 1){
+                res = res * a;
+            }
+            a = a * a;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
+typedef ModInt<1000000007> mint;
+MComb<1000000007> com(510000);
+MPow<1000000007> mpow;
 
 int main() {
-    ll n, a, b;
-    cin >> n >> a >> b;
-    ll all = 0;
-    all = modpow(2, n, MOD) - 1;
-    
-    
-    all = all - modncr(n, a);
-    if(all < 0) all += MOD;
-    all = all - modncr(n, b);
-    if(all < 0) all += MOD;
+    std::cout << std::fixed << std::setprecision(15);
+    ll n, a, b; cin >> n >> a >> b;
 
-    if(n == 2){
-        cout << 0 << endl;
-    }else{
-        cout << all << endl;
+    mint ans = 0;
+    ans = mpow.modpow(2, n) - 1;
+    mint tmp = 1;
+    for(ll i = 0; i < a; i++){
+        tmp = tmp * (n-i);
+        tmp = tmp / (a-i);
     }
-    
+    ans = ans - tmp;
+    tmp = 1;
+    for(ll i = 0; i < b; i++){
+        tmp = tmp * (n-i);
+        tmp = tmp / (b-i);
+    }
+    ans = ans - tmp;
+
+    cout << ans << endl;
+
 }

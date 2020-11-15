10ca5077a7d978552f6e06cdd3145ca17a83b12b
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2020-11-16 00:28:04 +0900

    20201116

---
+++ atcoder/abc/abc183/f.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+typedef long long ll;
+typedef long double ld;
+#define rep(i,n) for(ll i=0;i<(n);i++)
+#define repr(i,n) for(ll i=(n-1);i>=0;i--)
+#define all(x) x.begin(),x.end()
+#define br cout << "\n";
+using namespace std;
+using namespace atcoder;
+const long long INF = 1e10;
+const long long MOD = 1e9+7;
+using Graph = vector<vector<ll>>;
+using pll = pair<ll, ll>;
+template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
+template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
+ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b == 0) (a/b) + 1
+ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cout << v[i] << " ";} br;}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cout << v[i][j] << " ";} br;}}
+
+// 0 false, 1 true 
+// string number to int : -48
+// a to A : -32
+// ceil(a)  1.2->2.0
+// c++17	g++ -std=c++17 a.cpp
+// global vector -> 0 initialization
+// DONT FORGET TO INTIALIZE
+// If the result in local and judge is different, USE CODETEST!!
+
+struct UnionFind
+{
+    vector<long long> d;
+    UnionFind(long long n = 0) : d(n, -1)
+    {
+    }
+
+    //root : -size, not root: root
+    long long root(long long x){
+        if(d[x] < 0){
+            return x; 
+        }
+        return d[x] = root(d[x]);
+    }
+
+    bool unite(long long x, long long y){
+        x = root(x);
+        y = root(y);
+        if(x == y){
+            return false;
+        }
+        if(d[x] > d[y]){
+            swap(x, y);
+        }
+        d[x] += d[y];
+        d[y] = x;
+        return true;
+    }
+
+    long long size(long long x){
+        return -d[root(x)];
+    }
+
+    bool issame(long long a, long long b){
+        return root(a) == root(b);
+    }
+};
+
+int main() {
+    std::cout << std::fixed << std::setprecision(15);
+    ll n, q; cin >> n >> q;
+    vector<ll> c(n);
+    vector<map<ll, ll>> cla(n);
+    rep(i, n){
+        cin >> c[i];
+        cla[i][c[i]]++;
+    }
+    UnionFind uf(n);
+
+    rep(i, q){
+        ll x, y, z; cin >> x >> y >> z;
+        if(x == 1){
+            y--; z--;
+            if(!uf.issame(y, z)){
+                ll y_root = uf.root(y);
+                ll z_root = uf.root(z);
+                uf.unite(y, z);
+                ll after_root = uf.root(y);
+                if(after_root == y_root){
+                    //zからy
+                    for(auto j : cla[z_root]){
+                        cla[after_root][j.first] += j.second;
+                    }
+                }else{
+                    //yからz
+                    for(auto j : cla[y_root]){
+                        cla[after_root][j.first] += j.second;
+                    }
+                }
+            }
+        }else{
+            y--;
+            cout << cla[uf.root(y)][z] << endl;
+        }
+    }
+
+}

4ea8bbeeb74cfadc30d0cfd4d5cd2c4b9de3e1c9
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2021-05-31 01:03:27 +0900

    20210531

--- atcoder/arc/arc121/arc121/b/main.cpp
+++ atcoder/arc/arc121/arc121/b/main.cpp
@@ -31,8 +31,85 @@ using P = pair<ll, ll>;
 // If the result in local and judge is different, USE CODETEST!!
 // (a * b)over flow?   if(a > INF / b){ /* overflow */}
 
+void calc(const vector<ll> &x, const vector<ll> &y, const vector<ll> &z)
+{
+    ll ans = INF;
+
+    //奇数1 * 奇数2
+    for(ll i = 0; i < x.size(); i++){
+        ll dis = lower_bound(y.begin(), y.end(), x[i]) - y.begin();
+        ll tmp;
+        if(dis == 0){
+            tmp = abs(x[i] - y[0]);
+        }else if(dis == y.size()){
+            tmp = abs(x[i] - y[y.size() - 1]);
+        }else{
+            tmp = min( abs(x[i] - y[dis]), abs(x[i] - y[dis - 1]));
+        }
+        ans = min(ans, tmp);
+    }
+
+    //奇数1 * 偶数, 奇数2 * 偶数
+    if(z.size() != 0){
+        ll xxx = INF;
+        for(ll i = 0; i < x.size(); i++){
+            ll dis = lower_bound(z.begin(), z.end(), x[i]) - z.begin();
+            ll tmp;
+            if(dis == 0){
+                tmp = abs(x[i] - z[0]);
+            }else if(dis == z.size()){
+                tmp = abs(x[i] - z[z.size() - 1]);
+            }else{
+                tmp = min( abs(x[i] - z[dis]), abs(x[i] - z[dis - 1]));
+            }
+            xxx = min(xxx, tmp);
+        }
+        
+        ll yyy = INF;
+        for(ll i = 0; i < y.size(); i++){
+            ll dis = lower_bound(z.begin(), z.end(), y[i]) - z.begin();
+            ll tmp;
+            if(dis == 0){
+                tmp = abs(y[i] - z[0]);
+            }else if(dis == z.size()){
+                tmp = abs(y[i] - z[z.size() - 1]);
+            }else{
+                tmp = min( abs(y[i] - z[dis]), abs(y[i] - z[dis - 1]));
+            }
+            yyy = min(yyy, tmp);
+        }
+        ans = min(ans, xxx+yyy);
+    }
+    cout << ans << endl;
+}
+
 int main() {
     std::cout << std::fixed << std::setprecision(15);
+    ll n; cin >> n;
+    vector<ll> r;
+    vector<ll> g;
+    vector<ll> b;
+    rep(i, 2*n){
+        ll a; char c; cin >> a >> c;
+        if(c == 'R') r.push_back(a);
+        if(c == 'G') g.push_back(a);
+        if(c == 'B') b.push_back(a);
+    }
+    sort(all(r)); sort(all(g)); sort(all(b));
+
+    if(r.size() % 2 == 0 && g.size() % 2 == 0 && b.size() % 2 == 0){
+        cout << 0 << endl; return 0;
+    }
+
     
+    if(r.size() % 2 == 1 && g.size() % 2 == 1){
+        calc(r, g, b);
+    }
+    if(r.size() % 2 == 1 && b.size() % 2 == 1){
+        calc(r, b, g);
+    }
+    if(g.size() % 2 == 1 && b.size() % 2 == 1){
+        calc(g, b, r);
+    }
 
 }

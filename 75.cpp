b4574fdafc022f91f80d6eea453e4f68d7522d0b
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2021-07-03 19:15:48 +0900

    20210703

--- 75.cpp
+++ 75.cpp
@@ -32,8 +32,77 @@ const double PI = acos(-1);
 // If the result in local and judge is different, USE CODETEST!!
 // (a * b)over flow?   if(a > INF / b){ /* overflow */}
 
+struct PrimeNumber
+{
+    //O(sqrt(n))
+    //sosu hantei
+    bool is_prime(long long n){
+        for(long long i = 2; i * i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return n != 1;
+    }
+
+    //O(sqrt(n))  isn't sorted
+    //yakusu rekkyo
+    vector<long long> divisor(long long n){
+        vector<long long> res;
+        for(long long i = 1; i * i <= n; i++){
+            if(n % i == 0){
+                res.push_back(i);
+                if(i != n / i) res.push_back(n / i);
+            }
+        }
+        return res;
+    }
+
+    //O(sqrt(n))
+    //soinsu bunkai
+    map<long long, long long> prime_factor(long long n){
+        map<long long, long long> res;
+        for(long long i = 2; i * i <= n; i++){
+            while(n % i == 0){
+                n /= i;
+                res[i]++;
+            }
+        }
+        if(n != 1) res[n] = 1;
+        return res;
+    }
+
+    //O(n log log n)
+    //n madeno sosu rekkyo
+    vector<long long> eratosthenes(long long n){
+        vector<long long> prime;
+        vector<bool> is_prime(n + 1, true);
+        long long p = 0;
+        is_prime[0] = false; is_prime[1] = false;
+        for(long long i = 2; i <= n; i++){
+            if(is_prime[i]) prime.push_back(i);
+            for(long long j = 2 * i; j <= n; j += i) is_prime[j] = false;
+        }
+        return prime;
+    }
+};
+
 int main() {
     std::cout << std::fixed << std::setprecision(15);
-    
+    ll n; cin >> n;
+    PrimeNumber p;
+    map<ll, ll> mp = p.prime_factor(n);
+    ll res = 0;
+    for(auto i : mp){
+        res += i.second;
+    }
+    ll ans = 0;
+    ll cnt = 0;
+    while(true){
+        if(res <= POW(2, cnt)){
+            break;
+        }
+        ans++;
+        cnt++;
+    }
+    cout << ans << endl;
 
 }

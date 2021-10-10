1baed3d25c32ec206d59f4ba523def53e4f24cfd
zaurus-yusya <54097621+zaurus-yusya@users.noreply.github.com>
2021-10-11 00:15:15 +0900

    20211011

--- atcoder/abc/abc222/abc222/f/main.cpp
+++ atcoder/abc/abc222/abc222/f/main.cpp
@@ -6,19 +6,20 @@ typedef long double ld;
 #define rep(i,n) for(ll i=0;i<(n);i++)
 #define repr(i,n) for(ll i=(n-1);i>=0;i--)
 #define all(x) x.begin(),x.end()
-#define br cout << "\n";
+#define br cout << '\n';
 using namespace std;
-const long long INF = 1e18;
+const long long INF = 8e18;
 const long long MOD = 1e9+7;
 using Graph = vector<vector<ll>>;
 template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
 template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
 ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b != 0) (a/b) + 1
 ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
-template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cerr << v[i] << " ";} br;}
-template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cerr << v[i][j] << " ";} br;}}
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cerr << v[i] << ' ';} cerr << '\n';}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cerr << v[i][j] << ' ';} cerr << '\n';}}
 ll POW(ll a, ll n){ ll res = 1; while(n > 0){ if(n & 1){ res = res * a; } a *= a; n >>= 1; } return res; }
 using P = pair<ll, ll>;
+const double PI = acos(-1);
 
 // 0 false, 1 true 
 // string number to int : -48 or - '0'
@@ -31,8 +32,105 @@ using P = pair<ll, ll>;
 // If the result in local and judge is different, USE CODETEST!!
 // (a * b)over flow?   if(a > INF / b){ /* overflow */}
 
+//dijkstra
+struct Edge
+{
+    ll to, cost;
+    Edge(ll to = 0, ll cost = 0) : to(to), cost(cost) {}
+    //g[a].emplace_back(x, y) : x = to, y = cost
+};
+vector<ll> dist; //distance
+vector<ll> pre; //route restoration
+void dijkstra(vector<vector<Edge>> &g, ll start){
+    dist.assign(g.size(), INF);
+    pre.assign(g.size(), -1);
+    priority_queue<P, vector<P>, greater<P>> que;
+    dist[start] = 0;
+    que.push({0, start});
+
+    while(!que.empty()){
+        auto[d, v] = que.top(); que.pop();
+        if(dist[v] != d) continue;
+        for(Edge& e : g[v]){
+            ll next = d + e.cost; // next cost
+            if(dist[e.to] <= next){
+                continue;
+            }else{
+                dist[e.to] = next;
+                que.push({next, e.to});
+                pre[e.to] = v;
+            }
+        }
+    } 
+}
+void route_restoration(vector<ll> &pre, ll goal){
+    ll now = goal;
+    while(true){
+        cout << now << endl;
+        if(pre[now] == -1) break;
+        now = pre[now];
+    }
+}
+
 int main() {
     std::cout << std::fixed << std::setprecision(15);
-    
+    ll n; cin >> n;
+    vector<vector<Edge>> vec(n);
+    rep(i, n-1){
+        ll a, b, c; cin >> a >> b >> c; a--; b--;
+        vec[a].push_back({b, c});
+        vec[b].push_back({a, c});
+    }
+    vector<long long> d(n);
+    for(long long i = 0; i < n; i ++){
+        cin >> d[i];
+    }
+
+    //直径
+    ll s = 0, t = 0;
+    dijkstra(vec, 0);
+
+    //最も遠い点
+    ll cost = -1;
+    rep(i, n){
+        if(i == s) continue;
+        if(dist[i] + d[i] > cost){
+            cost = dist[i] + d[i];
+            t = i;
+        }
+    }
+
+    ll u = 0;
+    dijkstra(vec, t);
+    cost = -1;
+    rep(i, n){
+        if(i == t) continue;
+        if(dist[i] + d[i] > cost){
+            cost = dist[i] + d[i];
+            u = i;
+        }
+    }
+
+    vector<ll> distt = dist;
+
+    dijkstra(vec, u);
+    vector<ll> distu = dist;
+
+    //cout << t << " " << u << endl;
+    rep(i, n){
+
+        ll ans = -1;
+
+        if(i != t){
+            ans = max(ans, distt[i] + d[t]);
+        }
+        if(i != u){
+            ans = max(ans, distu[i] + d[u]);
+        }
+
+        cout << ans << '\n';
+
+    }
+
 
 }
--- atcoder/abc/abc222/f.cpp
+++ atcoder/abc/abc222/f.cpp
@@ -32,8 +32,105 @@ const double PI = acos(-1);
 // If the result in local and judge is different, USE CODETEST!!
 // (a * b)over flow?   if(a > INF / b){ /* overflow */}
 
+//dijkstra
+struct Edge
+{
+    ll to, cost;
+    Edge(ll to = 0, ll cost = 0) : to(to), cost(cost) {}
+    //g[a].emplace_back(x, y) : x = to, y = cost
+};
+vector<ll> dist; //distance
+vector<ll> pre; //route restoration
+void dijkstra(vector<vector<Edge>> &g, ll start){
+    dist.assign(g.size(), INF);
+    pre.assign(g.size(), -1);
+    priority_queue<P, vector<P>, greater<P>> que;
+    dist[start] = 0;
+    que.push({0, start});
+
+    while(!que.empty()){
+        auto[d, v] = que.top(); que.pop();
+        if(dist[v] != d) continue;
+        for(Edge& e : g[v]){
+            ll next = d + e.cost; // next cost
+            if(dist[e.to] <= next){
+                continue;
+            }else{
+                dist[e.to] = next;
+                que.push({next, e.to});
+                pre[e.to] = v;
+            }
+        }
+    } 
+}
+void route_restoration(vector<ll> &pre, ll goal){
+    ll now = goal;
+    while(true){
+        cout << now << endl;
+        if(pre[now] == -1) break;
+        now = pre[now];
+    }
+}
+
 int main() {
     std::cout << std::fixed << std::setprecision(15);
-    
+    ll n; cin >> n;
+    vector<vector<Edge>> vec(n);
+    rep(i, n-1){
+        ll a, b, c; cin >> a >> b >> c; a--; b--;
+        vec[a].push_back({b, c});
+        vec[b].push_back({a, c});
+    }
+    vector<long long> d(n);
+    for(long long i = 0; i < n; i ++){
+        cin >> d[i];
+    }
+
+    //直径
+    ll s = 0, t = 0;
+    dijkstra(vec, 0);
+
+    //最も遠い点
+    ll cost = -1;
+    rep(i, n){
+        if(i == s) continue;
+        if(dist[i] + d[i] > cost){
+            cost = dist[i] + d[i];
+            t = i;
+        }
+    }
+
+    ll u = 0;
+    dijkstra(vec, t);
+    cost = -1;
+    rep(i, n){
+        if(i == t) continue;
+        if(dist[i] + d[i] > cost){
+            cost = dist[i] + d[i];
+            u = i;
+        }
+    }
+
+    vector<ll> distt = dist;
+
+    dijkstra(vec, u);
+    vector<ll> distu = dist;
+
+    //cout << t << " " << u << endl;
+    rep(i, n){
+
+        ll ans = -1;
+
+        if(i != t){
+            ans = max(ans, distt[i] + d[t]);
+        }
+        if(i != u){
+            ans = max(ans, distu[i] + d[u]);
+        }
+
+        cout << ans << '\n';
+
+    }
+
 
 }

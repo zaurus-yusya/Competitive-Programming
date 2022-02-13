8e217a127b907c29b0356627e056d65c308c94ee
zaurus-yusya <gbshisan@gmail.com>
2022-02-14 00:41:55 +0900

    20220214

---
+++ atcoder/arc/arc135/arc135/b/main.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+// using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define rep(i,n) for(ll i=0;i<(n);i++)
+#define repr(i,n) for(ll i=(n-1);i>=0;i--)
+#define all(x) x.begin(),x.end()
+#define br cout << '\n';
+using namespace std;
+const long long INF = 8e18;
+using Graph = vector<vector<ll>>;
+template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
+template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
+ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b != 0) (a/b) + 1
+ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cerr << v[i] << ' ';} cerr << '\n';}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cerr << v[i][j] << ' ';} cerr << '\n';}}
+ll POW(ll a, ll n){ ll res = 1; while(n > 0){ if(n & 1){ res = res * a; } a *= a; n >>= 1; } return res; }
+using P = pair<ll, ll>;
+const double PI = acos(-1);
+
+// 0 false, 1 true 
+// string number to int : -48 or - '0'
+// a to A : -32
+// ceil(a)  1.2->2.0
+// c++17	g++ -std=c++17 a.cpp
+// global vector -> 0 initialization
+// DONT FORGET TO INTIALIZE
+// The type of GRID is CHAR. DONT USE STRING
+// If the result in local and judge is different, USE CODETEST!!
+// (a * b)over flow?   if(a > INF / b){ /* overflow */}
+// for(auto& i: mp) &&&&&&&&&&&&&
+
+int main() {
+    std::cout << std::fixed << std::setprecision(15);
+    ll n; cin >> n;
+    vector<long long> s(n);
+    for(long long i = 0; i < n; i ++){
+        cin >> s[i];
+    }
+
+    //1回目
+    vector<ll> a(n+2);
+    ll mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+    //vecdbg(a);
+    if(mi < 0){
+        ll x = -1 * mi;
+        a[0] = x;
+        a[2] = a[2] - x;
+    }
+
+    //2回目
+    mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+    //vecdbg(a);
+    if(mi < 0){
+        ll x = -1 * mi;
+        a[1] = x;
+        a[2] = a[2] - x;
+    }
+
+    //3回目
+    mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+    //vecdbg(a);
+
+    if(mi < 0){
+        //cout << "No" << endl;
+        //vecdbg(a);
+    }else{
+        cout << "Yes" << endl;
+        rep(i, n+2){
+            if(i < n+1){
+                cout << a[i] << " ";
+            }else{
+                cout << a[i];
+            }
+            
+        }br;
+        return 0;
+    }
+
+    a.assign(n+2, 0);
+
+    //1回目
+    mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+    //vecdbg(a);
+    if(mi < 0){
+        ll x = -1 * mi;
+        a[1] = x;
+        a[2] = a[2] - x;
+    }
+    mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+    //vecdbg(a);
+    if(mi < 0){
+        ll x = -1 * mi;
+        a[0] = x;
+        a[2] = a[2] - x;
+    }
+    //3回目
+    mi = 0;
+    for(ll i = 0; i < n; i++){
+        ll tmp = a[i] + a[i+1];
+        a[i+2] = s[i] - tmp;
+        mi = min(mi, a[i+2]);
+    }
+
+    if(mi < 0){
+        cout << "No" << endl;
+        //vecdbg(a);
+    }else{
+        cout << "Yes" << endl;
+        rep(i, n+2){
+            if(i < n+1){
+                cout << a[i] << " ";
+            }else{
+                cout << a[i];
+            }
+            
+        }br;
+        return 0;
+    }
+
+
+
+
+}

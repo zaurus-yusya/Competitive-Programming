1f4daaf98d89e792a72321c738f1121f52ab13b7
zaurus-yusya <gbshisan@gmail.com>
2022-04-22 00:11:25 +0900

    20220422

---
+++ codeforces/round783_div2/b.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+// using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define rep(i,n) for(ll i=0;i<(n);i++)
+#define repr(i,n) for(ll i=(n-1);i>=0;i--)
+#define all(x) x.begin(),x.end()
+#define br cout << '\n';
+using namespace std;
+const long long INF = 8e18;
+using Graph = vector<vector<ll>>;
+template<class T> inline bool chmin(T &a, T b) { if(a > b){ a = b; return true;} return false;}
+template<class T> inline bool chmax(T &a, T b) { if(a < b){ a = b; return true;} return false;}
+ll ceilll(ll a, ll b) {return (a + b-1) / b;} // if(a%b != 0) (a/b) + 1
+ll get_digit(ll a) {ll digit = 0; while(a != 0){a /= 10; digit++;} return digit;} // a != 0
+template<typename T> void vecdbg(vector<T>& v){ rep(i, v.size()){cerr << v[i] << ' ';} cerr << '\n';}
+template<typename T> void vecvecdbg(vector<vector<T>>& v){ rep(i, v.size()){rep(j, v[i].size()){cerr << v[i][j] << ' ';} cerr << '\n';}}
+ll POW(ll a, ll n){ ll res = 1; while(n > 0){ if(n & 1){ res = res * a; } a *= a; n >>= 1; } return res; }
+using P = pair<ll, ll>;
+const double PI = acos(-1);
+
+// 0 false, 1 true 
+// string number to int : -48 or - '0'
+// a to A : -32
+// ceil(a)  1.2->2.0
+// c++17	g++ -std=c++17 a.cpp
+// global vector -> 0 initialization
+// DONT FORGET TO INTIALIZE
+// The type of GRID is CHAR. DONT USE STRING
+// If the result in local and judge is different, USE CODETEST!!
+// (a * b)over flow?   if(a > INF / b){ /* overflow */}
+
+int main() {
+    std::cout << std::fixed << std::setprecision(15);
+    ll t; cin >> t;
+    rep(T, t){
+        ll n, m; cin >> n >> m;
+        vector<long long> a(n);
+        for(long long i = 0; i < n; i ++){
+            cin >> a[i];
+        }
+        sort(all(a), greater<ll>());
+
+        ll l = 0, lp = 0;
+        ll r = m, rp = 0;
+        string ans = "YES";
+        rep(i, n){
+            //cerr << "a[i] = " << a[i] << endl;
+            if(i == 0){
+                //cerr << "sit 0" << endl;
+                l += a[i]; lp = a[i];
+                r -= a[i]; rp = a[i];
+            }else if(lp > rp){
+                // lに座らせる
+                //cerr << "sit l" << endl;
+                if(l + 1 >= r){
+                    ans = "NO"; break;
+                }
+                l += a[i] + 1; lp = a[i];
+            }else{
+                // rに座らせる
+                if(r - 1 <= l){
+                    ans = "NO"; break;
+                }
+                //cerr << "sit r" << endl;
+                r -= a[i] + 1; rp = a[i];
+            }
+            
+            //cerr << "l = " << l << " r = " << r << endl;
+        }
+
+        cout << ans << endl;
+
+    }
+
+}
